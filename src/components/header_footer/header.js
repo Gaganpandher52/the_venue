6477ae4899738e52c70c8511b98bef20a5afda26
G_Pandher52 <gaganpandher@gagans-MacBook-Pro.local>
2019-07-16 14:37:33 -0600

    added side Drawer

--- src/components/header_footer/header.js
+++ src/components/header_footer/header.js
@@ -3,8 +3,18 @@ import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
+import SideDrawer from './sideDrawer'; 
 
 class Header extends Component {
+  state = {
+      drawOpen:false
+  }
+  toggleDrawer = (value ) => {
+    this.setState({
+      drawOpen: value
+    })
+  }
+
   render() {
     return (
       <div>
@@ -22,8 +32,22 @@ class Header extends Component {
           <div className='header_logo'>
             <div className='font_righteous header_logo_venue'>The Venue</div>
             <div className='header_logo_title'>Musical events</div>
-
           </div>
+          <IconButton
+            aria-label='Menu'
+            color='inherit'
+            onClick={()=> this.toggleDrawer(true)}
+          
+          >
+            <MenuIcon>
+              
+            </MenuIcon>
+          
+          </IconButton>
+          <SideDrawer
+            open={this.state.drawOpen}
+            onClose={(value)=> this.toggleDrawer(value)}
+          />
         </ToolBar>
         
         </AppBar>
