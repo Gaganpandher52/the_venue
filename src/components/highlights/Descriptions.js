3be3adf25075a86f203367d9e165086a49eee616
gaganpandher52 <gaganpandher52@gmail.com>
2019-07-25 16:05:29 -0600

    added new highlights component

---
+++ src/components/highlights/Descriptions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Fade from'react-reveal/Fade'
+
+const Descriptions = () => {
+  return (
+    <div className='center_wrapper'>
+      <h2>Highlights</h2>
+      <div className='highlight_description'>
+        For the second year, GitHub Universe is taking place at the Palace of Fine Arts—the historic site of the 1915 Panama-Pacific Exposition and one of the most photographed buildings in the city. After World War II, it was also a military storage depot, a warehouse for the Parks Department, and even a telephone book distribution center.
+      </div>
+      
+    </div>
+  );
+};
+
+export default Descriptions;
---
+++ src/components/highlights/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Description from './Descriptions'
+
+
+const Highlights = () => {
+  return (
+    <div className='highlights_wrapper'>
+      <Description/>
+      
+    </div>
+  );
+};
+
+export default Highlights;
