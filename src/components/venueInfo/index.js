a54fee911fa1dfba35d8c9ab9f0578378ab0f727
gaganpandher52 <gaganpandher52@gmail.com>
2019-07-28 00:01:24 -0600

    testing new components

--- src/components/venueInfo/index.js
+++ src/components/venueInfo/index.js
@@ -26,7 +26,7 @@ const VenueInfo = () => {
                     Event Date & Time
                   </div>        
                   <div className='vn_desc'>
-                    15 August 2019 @10.00 pm 
+                    15 August 2019 @10.00 pm  
                   </div> 
 
                 </div>
